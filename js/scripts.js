e8cdfb3be20d28390e52e10fe70bb04b7ebf58c4
Cyci25 <c2muriithi@gmail.com>
2018-07-13 18:39:00 +0300

    The numbers execute accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-$( document ).ready(function() {
+
+// user interface logic
+$(document).ready(function(){
+    $("a#button").click(function(event){
+        event.preventDefault();
+        $("#display").empty();
+        var userinput = parseInt($("input#number").val())
+        console.log(userinput);
+        var range=[...Array(userinput).keys()];
+        loopist(range);
+        console.log(range);
+    });
+});
+function print(val) {    
+    $("#display").append("<li>"+val+"</li>");
+    
+}
+
+
+
+// business interface
+function loopist(userinput) {
+    var results="";
+    userinput.forEach(y => {
+        y++
+        if (y % 15 === 0) {
+            print("pingpong");
+        }
+        else if (y % 5 === 0) {
+            print("pong");
+        
+        }
+        else if (y % 3 === 0) {
+            print("ping");
+            
+        }
+        else {
+            print (y)
+        } 
+    });
+}
+
